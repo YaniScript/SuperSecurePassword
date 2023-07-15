631c77fbd4433b0e99bf3fd31f6d37db0a82ad12
Dayana Montgomery <dayanamontgomery@Dayanas-MacBook-Air.local>
2023-07-15 13:31:05 -0400

    updated and organized the javascript

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-//The code is laid out by first clicking the "Generate Paswword Button"
-
-// Assignment Code
+// Assignment Code -- these are all the variables I created so that I can call on them throughout the function
 var generateBtn = document.querySelector("#generate");
 var lowercase
+var uppercase
+var numbers
+var specialCharacters
+var passwordLength
+var result = [lowercase, uppercase, numbers, specialCharacters, passwordLength,]
+var passwordSpecifics = promptMessage.passwordLength
+
 // Add event listener to generate button 
 generateBtn.addEventListener("click", promptMessage); //changed from WritePassword**
 
 function promptMessage() {
-  var userQuestions = alert('Please choose the specifics for your password');
-  lowercase = confirm('Do you want Lowercase letters')
-  console.log(lowercase)
+  alert('Please choose the specifics for your password');
+    lowercase = confirm('Do you want Lowercase letters?');
+    uppercase = confirm('Do you want Uppercase letters?');
+    numbers = confirm('Do you want numbers?');
+    specialCharacters = confirm('Do you want special characters?');
+    passwordLength = prompt('How long would you like your password to be?');
+  }
+
+if (passwordSpecifics <= 8 && >= 128) {
+  
+}
+
+
+ 
+
+
+
+  
 
 
   // // alert(userQuestions).addEventListener ("click", promptSpecificMessage);
   //   function promptSpecificMessage() {
   //     let passwordSpecifics = prompt('Enter the desired length of your password');
-    }
+
   // }
 
 
