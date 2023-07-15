35c1f06c9808030721a777fc082b5deb870b07b8
Dayana Montgomery <dayanamontgomery@Dayanas-MacBook-Air.local>
2023-07-15 12:24:22 -0400

    added the files for the project

---
+++ script.js
@@ -0,0 +1,73 @@
+//The code is laid out by first clicking the "Generate Paswword Button"
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowercase
+// Add event listener to generate button 
+generateBtn.addEventListener("click", promptMessage); //changed from WritePassword**
+
+function promptMessage() {
+  var userQuestions = alert('Please choose the specifics for your password');
+  lowercase = confirm('Do you want Lowercase letters')
+  console.log(lowercase)
+
+
+  // // alert(userQuestions).addEventListener ("click", promptSpecificMessage);
+  //   function promptSpecificMessage() {
+  //     let passwordSpecifics = prompt('Enter the desired length of your password');
+    }
+  // }
+
+
+// let response = promptMessage.alert (
+//   console.log("I've been clicked")
+// )
+
+
+  // let result = alert.addEventListener("click", (e)=>{
+  //   if(result === true) {
+  //     prompt('Enter the desired length of your password')
+  //   }
+  // })
+
+//   if (result === true) {
+//     prompt('Enter the desired length of your password')
+//   } else (result === false) {
+    
+//   }
+
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button -- idk what this code below does, so I'm going to comment it out.
+// // generateBtn.addEventListener("click", writePassword);
+
